2145ef548dbe804b9db00b67d89d3ac9b57558cd
sergiblanch <sergiblanch@3b39871e-fe4b-0410-b5db-e82997f68959>
2013-10-28 12:21:20 +0000

    add attributes for the RF generator
    git-svn-id: svn+ssh://svn.code.sf.net/p/tango-ds/code/DeviceClasses/Communication/Skippy/trunk@15975 3b39871e-fe4b-0410-b5db-e82997f68959

--- src/instructions/radioFrequencyGenerator/rohdeSchwarzRFG.py
+++ src/instructions/radioFrequencyGenerator/rohdeSchwarzRFG.py
@@ -21,3 +21,84 @@
 ##        (c) - Controls Software Section - ALBA/CELLS
 ##############################################################################
 
+import PyTango
+
+Attribute('Frequency',
+          {'type':PyTango.CmdArgType.DevDouble,
+           'dim':0,
+           'readCmd':":FREQ?",
+           'writeCmd':lambda value:":FREQ %s"%(str(value))
+         })
+
+Attribute('PowerLevel',
+          {'type':PyTango.CmdArgType.DevDouble,
+           'dim':0,
+           'readCmd':":POW?",
+           'writeCmd':lambda value:":POW %s"%(str(value))
+         })
+
+Attribute('RfState',
+          {'type':PyTango.CmdArgType.DevBoolean,
+           'dim':0,
+           'readCmd':":OUTP:STAT?",
+           'writeCmd':lambda value:":OUTP:STAT %s"%(value),
+         })
+
+Attribute('FrequencyRangeLow',
+          {'type':PyTango.CmdArgType.DevDouble,
+           'dim':0,
+           'readCmd':":FREQ:PHAS:CONT:LOW?",
+           'writeCmd':lambda value:":FREQ:PHAS:CONT:LOW %s"%(str(value))
+         })
+
+Attribute('FrequencyRangeHigh',
+          {'type':PyTango.CmdArgType.DevDouble,
+           'dim':0,
+           'readCmd':":FREQ:PHAS:CONT:HIGH?",
+           'writeCmd':lambda value:":FREQ:PHAS:CONT:HIGH %s"%(str(value))
+         })
+
+Attribute('PowerRangeLow',
+          {'type':PyTango.CmdArgType.DevDouble,
+           'dim':0,
+           'readCmd':"::OUTP:AFIX:RANG:LOW?",
+           'writeCmd':lambda value:":OUTP:AFIX:RANG:LOW %s"%(str(value))
+         })
+
+Attribute('PowerRangeHigh',
+          {'type':PyTango.CmdArgType.DevDouble,
+           'dim':0,
+           'readCmd':":OUTP:AFIX:RANG:UPP?",
+           'writeCmd':lambda value:":OUTP:AFIX:RANG:UPP %s"%(str(value))
+         })
+
+Attribute('Impedancy',
+          {'type':PyTango.CmdArgType.DevShort,
+           'dim':[1,100],
+           'readCmd':":OUTP:IMP?",
+         })
+
+Attribute('PhaseContinuousFrequencyActive',
+          {'type':PyTango.CmdArgType.DevBoolean,
+           'dim':0,
+           'readCmd':":FREQ:PHAS:CONT:STAT?"
+         })
+
+Attribute('PhaseContinuousFrequencyNarrow',
+          {'type':PyTango.CmdArgType.DevString,
+           'dim':0,
+           'readCmd':":FREQ:PHAS:CONT:MODE?"
+         })
+
+Attribute('ExtOscSrc',
+          {'type':PyTango.CmdArgType.DevBoolean,
+           'dim':0,
+           'readCmd':":ROSCillator:SOURce?",
+           'writeCmd':lambda value:":ROSCillator:SOURce %s"%(str(value))
+         })
+
+Attribute('Errors',
+          {'type':PyTango.CmdArgType.DevString,
+           'dim':[1,100],
+           'readCmd':":SYST:SERROR?",
+         })
