70c0cfdeb8713ea503e9a4fc1d1dc011b977afae
sergiblanch <sergiblanch@3b39871e-fe4b-0410-b5db-e82997f68959>
2013-10-28 12:21:38 +0000

    allow no parent, and then tune the debug_stream document functions extend the socket recv method to work when the request to the instrument is an spectrum implement and add mutex to the ask_for_values
    git-svn-id: svn+ssh://svn.code.sf.net/p/tango-ds/code/DeviceClasses/Communication/Skippy/trunk@15977 3b39871e-fe4b-0410-b5db-e82997f68959

--- src/communicator.py
+++ src/communicator.py
@@ -30,12 +30,15 @@ class Communicator:
     def __init__(self):
         raise NotImplementedError("This class is pure abstract")
 
+    def debug_stream(self,msg):
+        if hasattr(self,'__parent') and not self.__parent == None:
+            self.__parent.debug_stream(msg)
+        else:
+            print(msg)
+
     def ask(self, commandList):
-        """
-            A combination of send(msg) and recv()
-            @TODO: to make it work, you need to concatenate the request for 
-            change and the query in the command.
-        """
+        '''Prepare the command list and do a combination of send(msg) and recv()
+        '''
         self.mutex.acquire()
         command = self.prepareCommand(commandList)
         self._send(command)
@@ -44,19 +47,27 @@ class Communicator:
         return answer
     
     def write(self,commandList):
+        '''Do a write operation to the remote
+        '''
         self.mutex.acquire()
         command = self.prepareCommand(commandList)
         self._send(command)
         self.mutex.release()
         
     def read(self):
+        '''Read if the remote have said something
+        '''
         self.mutex.acquire()
         answer = self._recv()
         self.mutex.release()
         return answer
 
     def prepareCommand(self,commands):
-        if isinstance(commands, str): 
+        '''Structure the command to the remote. If it's already an string, be
+           sure it ends with a '\n'. If it's a list of strings concatenate them
+           using ';' as a separator.
+        '''
+        if isinstance(commands, str) and not commands[-1:] == '\n':
             return commands+'\n'
         elif isinstance(commands, list): 
             return reduce(lambda x,y: x+';'+y+';',commands)[:-1]+'\n'
@@ -65,13 +76,13 @@ class Communicator:
 
 
 class bySocket(Communicator):
-    def __init__(self,hostName,port,parent):
+    def __init__(self,hostName,port=5025,parent=None):
         self.__hostName = hostName
         self.__port = port
         self.__parent = parent
         self.mutex = threading.Lock()
-        self.__parent.debug_stream("building a communication to %s by socket "\
-                                   "using port %d"%(self.__hostName,self.__port))
+        self.debug_stream("building a communication to %s by socket "\
+                          "using port %d"%(self.__hostName,self.__port))
         self.__sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.__sock.settimeout(1)
     def connect(self):
@@ -80,28 +91,44 @@ class bySocket(Communicator):
     def _send(self,msg):
         self.__sock.send(msg)
 
-    def _recv(self):
-        msg = self.__sock.recv(1024)
-        return msg
+    def _recv(self,bufsize=10240):
+        completeMsg = ''
+        buffer = self.__sock.recv(bufsize)
+        completeMsg = ''.join([completeMsg,buffer])
+        if completeMsg.startswith('#') or completeMsg.count(','):
+            #This is when what was requested is an array of values from one attr
+            #when binary mode starts with #, on ASCII mode the values are ',' separated
+            while not len(buffer) == 0:
+                try:
+                    buffer = self.__sock.recv(bufsize)
+                except Exception,e:
+                    #print("reception break due to exception: %s"%(e))
+                    break
+                    #FIXME: there must be a better stopper than a timeout
+                else:
+                    completeMsg = ''.join([completeMsg,buffer])
+        return completeMsg
 
     def close(self):
         self.__sock.close()
 
-    def ask_for_values(self, command):
-        """
-            Not sure about the difference between this and ask.
-            This is only used in PyScope ds.
-        """
-        raise NotImplementedError("Not implemented")
+    def ask_for_values(self, commandList):
+        '''
+        '''
+        self.mutex.acquire()
+        command = self.prepareCommand(commandList)
+        self._send(command)
+        answer = self._recv()
+        self.mutex.release()
+        return answer
 
 
 class byVisa(Communicator):
-    def __init__(self,devName,parent):
+    def __init__(self,devName,parent=None):
         self.__device = PyTango.DeviceProxy(devName)
         self.__parent = parent
         self.mutex = threading.Lock()
-        self.__parent.debug_stream("building a communication to %s by PyVisa"
-                                   %(devName))
+        self.debug_stream("building a communication to %s by PyVisa"%(devName))
 
     def connect(self):
         if not self.__device.State() == PyTango.DevState.ON:
@@ -120,6 +147,10 @@ class byVisa(Communicator):
     def ask(self, commandList):
         return array.array('B',self.__device.Ask(array.array('B',commandList).tolist())).tostring()
 
-    def ask_for_values(self, command):
-        return self.__visaSc__deviceope.AskValues(array.array('B',commandList).tolist())
+    def ask_for_values(self, commandList):
+        self.mutex.acquire()
+        answer = self.__device.AskValues(array.array('B',commandList).tolist())
+        self.debug_stream("byVisa.ask_for_values(): %s"%answer)
+        self.mutex.release()
+        return answer
 
