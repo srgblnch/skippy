b95c7a459ec2227ed92c5cd94a591c083a5092c1
sergiblanch <sergiblanch@3b39871e-fe4b-0410-b5db-e82997f68959>
2013-10-28 12:22:16 +0000

    the dim key in the attr description, is a list always, even in 0d its [0] add frequency as rampeable
    git-svn-id: https://svn.code.sf.net/p/tango-ds/code/DeviceClasses/Communication/Skippy/trunk@15982 3b39871e-fe4b-0410-b5db-e82997f68959

--- src/instructions/radioFrequencyGenerator/rohdeSchwarzRFG.py
+++ src/instructions/radioFrequencyGenerator/rohdeSchwarzRFG.py
@@ -25,51 +25,52 @@ import PyTango
 
 Attribute('Frequency',
           {'type':PyTango.CmdArgType.DevDouble,
-           'dim':0,
+           'dim':[0],
            'readCmd':":FREQ?",
-           'writeCmd':lambda value:":FREQ %s"%(str(value))
+           'writeCmd':lambda value:":FREQ %s"%(str(value)),
+           'rampeable':True,
          })
 
 Attribute('PowerLevel',
           {'type':PyTango.CmdArgType.DevDouble,
-           'dim':0,
+           'dim':[0],
            'readCmd':":POW?",
-           'writeCmd':lambda value:":POW %s"%(str(value))
+           'writeCmd':lambda value:":POW %s"%(str(value)),
          })
 
 Attribute('RfState',
           {'type':PyTango.CmdArgType.DevBoolean,
-           'dim':0,
+           'dim':[0],
            'readCmd':":OUTP:STAT?",
            'writeCmd':lambda value:":OUTP:STAT %s"%(value),
          })
 
 Attribute('FrequencyRangeLow',
           {'type':PyTango.CmdArgType.DevDouble,
-           'dim':0,
+           'dim':[0],
            'readCmd':":FREQ:PHAS:CONT:LOW?",
-           'writeCmd':lambda value:":FREQ:PHAS:CONT:LOW %s"%(str(value))
+           'writeCmd':lambda value:":FREQ:PHAS:CONT:LOW %s"%(str(value)),
          })
 
 Attribute('FrequencyRangeHigh',
           {'type':PyTango.CmdArgType.DevDouble,
-           'dim':0,
+           'dim':[0],
            'readCmd':":FREQ:PHAS:CONT:HIGH?",
-           'writeCmd':lambda value:":FREQ:PHAS:CONT:HIGH %s"%(str(value))
+           'writeCmd':lambda value:":FREQ:PHAS:CONT:HIGH %s"%(str(value)),
          })
 
 Attribute('PowerRangeLow',
           {'type':PyTango.CmdArgType.DevDouble,
-           'dim':0,
+           'dim':[0],
            'readCmd':"::OUTP:AFIX:RANG:LOW?",
-           'writeCmd':lambda value:":OUTP:AFIX:RANG:LOW %s"%(str(value))
+           'writeCmd':lambda value:":OUTP:AFIX:RANG:LOW %s"%(str(value)),
          })
 
 Attribute('PowerRangeHigh',
           {'type':PyTango.CmdArgType.DevDouble,
-           'dim':0,
+           'dim':[0],
            'readCmd':":OUTP:AFIX:RANG:UPP?",
-           'writeCmd':lambda value:":OUTP:AFIX:RANG:UPP %s"%(str(value))
+           'writeCmd':lambda value:":OUTP:AFIX:RANG:UPP %s"%(str(value)),
          })
 
 Attribute('Impedancy',
@@ -80,21 +81,21 @@ Attribute('Impedancy',
 
 Attribute('PhaseContinuousFrequencyActive',
           {'type':PyTango.CmdArgType.DevBoolean,
-           'dim':0,
-           'readCmd':":FREQ:PHAS:CONT:STAT?"
+           'dim':[0],
+           'readCmd':":FREQ:PHAS:CONT:STAT?",
          })
 
 Attribute('PhaseContinuousFrequencyNarrow',
           {'type':PyTango.CmdArgType.DevString,
-           'dim':0,
-           'readCmd':":FREQ:PHAS:CONT:MODE?"
+           'dim':[0],
+           'readCmd':":FREQ:PHAS:CONT:MODE?",
          })
 
 Attribute('ExtOscSrc',
           {'type':PyTango.CmdArgType.DevBoolean,
-           'dim':0,
+           'dim':[0],
            'readCmd':":ROSCillator:SOURce?",
-           'writeCmd':lambda value:":ROSCillator:SOURce %s"%(str(value))
+           'writeCmd':lambda value:":ROSCillator:SOURce %s"%(str(value)),
          })
 
 Attribute('Errors',
