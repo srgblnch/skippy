7e202b4d00ed09e1b33310f8588365fd41e77f19
Sergi Blanch-Torné <sergi@calcurco.cat>
2016-07-15 16:54:50 +0200

    prepare to have a tango device to connect to the fake instrument.

--- test/fakeInstrument.py
+++ test/fakeInstrument.py
@@ -25,21 +25,67 @@ __status__ = "Production"
 
 
 from instrIdn import InstrumentIdentification
+import PyTango
 import scpi
 import skippy
+import traceback
+
+
+DevName = 'fake/skyppy/instrument'
+DevClass = 'Skippy'
+DevServer = 'Skippy/FakeInstrument'
 
 
 class FakeInstrument(object):
     def __init__(self, *args, **kwargs):
         super(FakeInstrument, self).__init__(*args, **kwargs)
-        identity = InstrumentIdentification('ALBA', 'test', 0,
-                                            skippy.version())
+        self._identity = InstrumentIdentification('ALBA', 'FakeInstrument', 0,
+                                                  skippy.version())
         self._scpiObj = scpi.scpi(local=True)
-        self._scpiObj.addSpecialCommand('IDN', identity.idn)
+        self._scpiObj.addSpecialCommand('IDN', self._identity.idn)
+        # Prepare ...
+        self._scpiObj.open()
+
+
+def createTestDevice():
+    tangodb = PyTango.Database()
+    print("\nCreating a %s device in %s:%s"
+          % (DevName, tangodb.get_db_host(), tangodb.get_db_port()))
+    devInfo = PyTango.DbDevInfo()
+    devInfo.name = DevName
+    devInfo._class = DevClass
+    devInfo.server = DevServer
+    tangodb.add_device(devInfo)
+    print("Server %s added" % DevServer)
+    propertyName = 'Instrument'
+    propertyValue = 'localhost'
+    property = PyTango.DbDatum(propertyName)
+    property.value_string.append(propertyValue)
+    print("Set property %s: %s" % (propertyName, propertyValue))
+    tangodb.put_device_property(DevName, property)
+    # TODO: run the device server to be tested
+
+
+def deleteTestDevice():
+    try:
+        tangodb = PyTango.Database()
+        print("\nRemove a %s device in %s:%s"
+              % (DevName, tangodb.get_db_host(), tangodb.get_db_port()))
+        tangodb.delete_device(DevName)
+        tangodb.delete_server(DevServer)
+    except Exception as e:
+        print("\n* Deletion failed, please review manually for garbage *\n")
 
 
 def main():
     instrument = FakeInstrument()
+    try:
+        createTestDevice()
+    except Exception as e:
+        print("Cannot complete the test: %s" % e)
+        traceback.print_exc()
+    finally:
+        deleteTestDevice()
 
 
 if __name__ == '__main__':
