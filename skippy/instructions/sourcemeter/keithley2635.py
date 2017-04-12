11e120007c97ac4f9b39e5f914bcdb1c7b1c45d2
Sergi Blanch-Torné <sergi@calcurco.cat>
2017-04-12 12:59:59 +0200

    new attributes while working with the instrument has shown.

--- skippy/instructions/sourcemeter/keithley2635.py
+++ skippy/instructions/sourcemeter/keithley2635.py
@@ -34,7 +34,109 @@ Attribute('Output',
            % ("ON" if value else "OFF"),
            })
 
-Attribute('MeasureRange',
+# Source ---
+Attribute('SourceCurrentAutoRange',
+          {'type': PyTango.CmdArgType.DevBoolean,
+           'dim': [0],
+           'readCmd': "print(smua.source.autorangei)\n?",
+           'readFormula': "bool(float(VALUE))",
+           'writeCmd': lambda value: "smua.source.autorangei=smua.AUTORANGE_%s"
+           % ("ON" if value else "OFF"),
+           })
+
+Attribute('SourceVoltageAutoRange',
+          {'type': PyTango.CmdArgType.DevBoolean,
+           'dim': [0],
+           'readCmd': "print(smua.source.autorangev)\n?",
+           'readFormula': "bool(float(VALUE))",
+           'writeCmd': lambda value: "smua.source.autorangev=smua.AUTORANGE_%s"
+           % ("ON" if value else "OFF"),
+           })
+
+Attribute('SourceFunction',
+          {'type': PyTango.CmdArgType.DevString,
+           'dim': [0],
+           'readCmd': "print(smua.source.func)\n?",
+           'readFormula': "{0: 'AMPS', 1: 'VOLTS'}[int(float(VALUE))]",
+           'writeCmd': lambda value: "smua.source.func=smua.OUTPUT_DC%s" % value,
+           'writeValues': ['AMPS', 'VOLTS']
+           })
+
+Attribute('SourceCurrentLevel',
+          {'type': PyTango.CmdArgType.DevDouble,
+           'format': '%e',
+           'dim': [0],
+           'readCmd': "print(smua.source.leveli)\n?",
+           'writeCmd': lambda value: "smua.source.leveli=%s" % value,
+           })
+
+Attribute('SourceVoltageLevel',
+          {'type': PyTango.CmdArgType.DevDouble,
+           'format': '%e',
+           'dim': [0],
+           'readCmd': "print(smua.source.levelv)\n?",
+           'writeCmd': lambda value: "smua.source.levelv=%s" % value,
+           })
+
+Attribute('SourceVoltageLimit',
+          {'type': PyTango.CmdArgType.DevDouble,
+           'format': '%e',
+           'dim': [0],
+           'readCmd': "print(smua.source.limitv)\n?",
+           'writeCmd': lambda value: "smua.source.limitv=%s" % value,
+           })
+
+Attribute('SourceCurrentLimit',
+          {'type': PyTango.CmdArgType.DevDouble,
+           'format': '%e',
+           'dim': [0],
+           'readCmd': "print(smua.source.limiti)\n?",
+           'writeCmd': lambda value: "smua.source.limiti=%s" % value,
+           })
+
+Attribute('SourceCurrentRange',
+          {'type': PyTango.CmdArgType.DevDouble,
+           'format': '%e',
+           'dim': [0],
+           'readCmd': "print(smua.source.rangei)\n?",
+           'writeCmd': lambda value: "smua.source.rangei=%s" % value,
+           })
+
+Attribute('SourceVoltageRange',
+          {'type': PyTango.CmdArgType.DevDouble,
+           'format': '%e',
+           'dim': [0],
+           'readCmd': "print(smua.source.rangev)\n?",
+           'writeCmd': lambda value: "smua.source.rangev=%s" % value,
+           })
+
+Attribute('SourceLimitReached',
+          {'type': PyTango.CmdArgType.DevBoolean,
+           'dim': [0],
+           'readCmd': "print(smua.source.compliance)\n?",
+           'readFormula': '{"false": False, "true": True}[str(VALUE).strip()]'
+           })
+
+# Measure ---
+Attribute('MeasureCurrentAutoRange',
+          {'type': PyTango.CmdArgType.DevBoolean,
+           'dim': [0],
+           'readCmd': "print(smua.measure.autorangei)\n?",
+           'readFormula': "bool(float(VALUE))",
+           'writeCmd': lambda value: "smua.measure.autorangei=smua.AUTORANGE_%s"
+           % ("ON" if value else "OFF"),
+           })
+
+Attribute('MeasureVoltageAutoRange',
+          {'type': PyTango.CmdArgType.DevBoolean,
+           'dim': [0],
+           'readCmd': "print(smua.measure.autorangev)\n?",
+           'readFormula': "bool(float(VALUE))",
+           'writeCmd': lambda value: "smua.measure.autorangev=smua.AUTORANGE_%s"
+           % ("ON" if value else "OFF"),
+           })
+
+Attribute('MeasureCurrentRange',
           {'type': PyTango.CmdArgType.DevDouble,
            'format': '%e',
            'dim': [0],
@@ -42,10 +144,74 @@ Attribute('MeasureRange',
            'writeCmd': lambda value: "smua.measure.rangei=%s" % value,
            })
 
-Attribute('SourceRange',
+Attribute('MeasureVoltageRange',
           {'type': PyTango.CmdArgType.DevDouble,
            'format': '%e',
            'dim': [0],
-           'readCmd': "print(smua.source.rangei)\n?",
-           'writeCmd': lambda value: "smua.source.rangei=%s" % value,
+           'readCmd': "print(smua.measure.rangev)\n?",
+           'writeCmd': lambda value: "smua.measure.rangev=%s" % value,
+           })
+
+Attribute('MeasureCurrent',
+          {'type': PyTango.CmdArgType.DevDouble,
+           'dim': [0],
+           'readCmd': "print(smua.measure.i())\n?",
+           'readFormula': "float(VALUE)",
+           })
+
+Attribute('MeasureVoltage',
+          {'type': PyTango.CmdArgType.DevDouble,
+           'dim': [0],
+           'readCmd': "print(smua.measure.v())\n?",
+           'readFormula': "float(VALUE)",
+           })
+
+Attribute('MeasureResistance',
+          {'type': PyTango.CmdArgType.DevDouble,
+           'dim': [0],
+           'readCmd': "print(smua.measure.r())\n?",
+           'readFormula': "float(VALUE)",
+           })
+
+Attribute('MeasurePower',
+          {'type': PyTango.CmdArgType.DevDouble,
+           'dim': [0],
+           'readCmd': "print(smua.measure.p())\n?",
+           'readFormula': "float(VALUE)",
+           })
+
+Attribute('MeasureCurrentLevel',
+          {'type': PyTango.CmdArgType.DevDouble,
+           'format': '%e',
+           'dim': [0],
+           'readCmd': "print(smua.measure.leveli)\n?",
+           'readFormula': 'float(VALUE.replace("nil","nan"))',
+           'writeCmd': lambda value: "smua.measure.leveli=%s" % value,
+           })
+
+Attribute('MeasureVoltageLevel',
+          {'type': PyTango.CmdArgType.DevDouble,
+           'format': '%e',
+           'dim': [0],
+           'readCmd': "print(smua.measure.levelv)\n?",
+           'readFormula': 'float(VALUE.replace("nil","nan"))',
+           'writeCmd': lambda value: "smua.measure.levelv=%s" % value,
+           })
+
+Attribute('MeasureAutoZero',
+          {'type': PyTango.CmdArgType.DevString,
+           'dim': [0],
+           'readCmd': "print(smua.measure.autozero)\n?",
+           'readFormula': "{0: 'OFF', 1: 'ONCE', 2: 'AUTO'}[int(float(VALUE))]",
+           'writeCmd': lambda value: "smua.measure.autozero=smua.AUTOZERO_%s" % value,
+           'writeValues': ['OFF', 'ONCE', 'AUTO']
+           })
+
+# Others ---
+Attribute('LineFrequency',
+	  {'type': PyTango.CmdArgType.DevDouble,
+           'format': '%e',
+           'dim': [0],
+           'readCmd': "print(localnode.linefreq)\n?",
+           'writeCmd': lambda value: "localnode.linefreq=%s" % value,
            })
