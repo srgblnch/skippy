864fcb9154bb99a29234f4281bcd8049f88aac59
Sergi Blanch-Torne <sblanch@cells.es>
2017-07-14 15:35:19 +0200

    alphabetical sort to easyly chack what is already included.

--- skippy/instructions/scope/agilentDSO.py
+++ skippy/instructions/scope/agilentDSO.py
@@ -47,184 +47,6 @@ import PyTango
 #            )
 #
 
-Attribute('State',
-          {'type': PyTango.CmdArgType.DevBoolean,
-           'dim': [0],
-           'readCmd': lambda ch, num: ":%s%d:DISPlay?" % (ch, num),
-           'writeCmd': lambda ch, num: (lambda value: ":%s%d:DISPlay %s"
-                                        % (ch, num, 'ON' if value else 'OFF')),
-           'channels': True,
-           'functions': True,
-           'manager': True,
-           })
-
-Attribute('Impedance',
-          {'type': PyTango.CmdArgType.DevString,
-           'dim': [0],
-           'readCmd': lambda ch, num: ":%s%d:INPut?" % (ch, num),
-           'channels': True,
-           })
-
-Attribute('Amplitude',
-          {'type': PyTango.CmdArgType.DevDouble,
-           'format': '%9.6f',
-           'dim': [0],
-           'readCmd': lambda ch, num: ":MEAS:VAMP? %s%d" % (ch, num),
-           'channels': True,
-           'functions': True,
-           })
-
-Attribute('Scale',
-          {'type': PyTango.CmdArgType.DevDouble,
-           'format': '%9.6f',
-           'dim': [0],
-           'readCmd': lambda ch, num: ":%s%d:SCALe?" % (ch, num),
-           'writeCmd': lambda ch, num: (lambda value: ":%s%d:SCALe %s"
-                                        % (ch, num, value)),
-           'channels': True,
-           })
-
-Attribute('Offset',
-          {'type': PyTango.CmdArgType.DevDouble,
-           'format': '%9.6f',
-           'dim': [0],
-           'readCmd': lambda ch, num: ":%s%d:OFFS?" % (ch, num),
-           'writeCmd': lambda ch, num: (lambda value: ":%s%d:OFFS %s"
-                                        % (ch, num, value)),
-           'channels': True,
-           })
-
-Attribute('VPeakToPeak',
-          {'type': PyTango.CmdArgType.DevDouble,
-           'format': '%9.6f',
-           'dim': [0],
-           'readCmd': lambda ch, num: ":MEAS:VPP? %s%d" % (ch, num),
-           'channels': True,
-           'functions': True,
-           })
-
-Attribute('VoltageMin',
-          {'type': PyTango.CmdArgType.DevDouble,
-           'format': '%9.6f',
-           'dim': [0],
-           'readCmd': lambda ch, num: ":MEAS:VMIN? %s%d" % (ch, num),
-           'channels': True,
-           'functions': True,
-           })
-
-Attribute('VoltageMax',
-          {'type': PyTango.CmdArgType.DevDouble,
-           'format': '%9.6f',
-           'dim': [0],
-           'readCmd': lambda ch, num: ":MEAS:VMAX? %s%d" % (ch, num),
-           'channels': True,
-           'functions': True,
-           })
-
-Attribute('VoltageUpper',
-          {'type': PyTango.CmdArgType.DevDouble,
-           'format': '%9.6f',
-           'dim': [0],
-           'readCmd': lambda ch, num: ":MEAS:VUPPER? %s%d" % (ch, num),
-           'channels': True,
-           })
-
-Attribute('VoltageLower',
-          {'type': PyTango.CmdArgType.DevDouble,
-           'format': '%9.6f',
-           'dim': [0],
-           'readCmd': lambda ch, num: ":MEAS:VLOWER? %s%d" % (ch, num),
-           'channels': True,
-           })
-
-Attribute('Frequency',
-          {'type': PyTango.CmdArgType.DevDouble,
-           'format': '%9.6f',
-           'dim': [0],
-           'readCmd': lambda ch, num: ":MEAS:FREQ? %s%d" % (ch, num),
-           'channels': True,
-           })
-
-Attribute('Period',
-          {'type': PyTango.CmdArgType.DevDouble,
-           'format': '%9.6f',
-           'dim': [0],
-           'readCmd': lambda ch, num: ":MEAS:PERIOD? %s%d" % (ch, num),
-           'channels': True,
-           })
-
-Attribute('RiseTime',
-          {'type': PyTango.CmdArgType.DevDouble,
-           'format': '%9.6f',
-           'dim': [0],
-           'readCmd': lambda ch, num: ":MEAS:RISETIME? %s%d" % (ch, num),
-           'channels': True,
-           })
-
-Attribute('FallTime',
-          {'type': PyTango.CmdArgType.DevDouble,
-           'format': '%9.6f',
-           'dim': [0],
-           'readCmd': lambda ch, num: ":MEAS:FALLTIME? %s%d" % (ch, num),
-           'channels': True,
-           })
-
-Attribute('OverShoot',
-          {'type': PyTango.CmdArgType.DevDouble,
-           'format': '%9.6f',
-           'dim': [0],
-           'readCmd': lambda ch, num: ":MEAS:OVERSHOOT? %s%d" % (ch, num),
-           'channels': True,
-           })
-
-Attribute('PreShoot',
-          {'type': PyTango.CmdArgType.DevDouble,
-           'format': '%9.6f',
-           'dim': [0],
-           'readCmd': lambda ch, num: ":MEAS:PRESHOOT? %s%d" % (ch, num),
-           'channels': True,
-           })
-
-Attribute('Waveform',
-          {'type': PyTango.CmdArgType.DevDouble,
-           'format': '%9.6f',
-           'dim': [1, 40000000],
-           'readCmd': lambda ch, num: ":WAVeform:SOURce %s%d;:WAVeform:DATA?"
-                                      % (ch, num),
-           'channels': True,
-           'functions': True,
-           })
-
-Attribute('CurrentSampleRate',
-          {'type': PyTango.CmdArgType.DevDouble,
-           'format': '%9.6f',
-           'dim': [0],
-           'readCmd': ":ACQuire:SRATe?",
-           })
-
-Attribute('ScaleH',
-          {'type': PyTango.CmdArgType.DevDouble,
-           'format': '%9.6f',
-           'dim': [0],
-           'readCmd': ":TIMebase:SCALe?",
-           'writeCmd': lambda value: ":TIMebase:SCALe %s" % (str(value)),
-           })
-
-Attribute('OffsetH',
-          {'type': PyTango.CmdArgType.DevDouble,
-           'format': '%9.6f',
-           'dim': [0],
-           'readCmd': ":TIMebase:POS?",
-           'writeCmd': lambda value: ":TIMebase:POS %s" % (str(value)),
-           })
-
-Attribute('TriggerType',
-          {'type': PyTango.CmdArgType.DevString,
-           'dim': [0],
-           'readCmd': ":TRIGger:MODE?",
-           'writeCmd': lambda value: ":TRIGger:MODE %s" % (str(value)),
-           })
-
 Attribute('AcquisitionMode',
           {'type': PyTango.CmdArgType.DevString,
            'dim': [0],
@@ -239,28 +61,13 @@ Attribute('AcquisitonPoints',
            'writeCmd': lambda value: ":ACQuire:POINts %s" % (str(value)),
            })
 
-Attribute('WaveformDataFormat',
-          {'type': PyTango.CmdArgType.DevString,
-           'dim': [0],
-           'readCmd': ":waveform:format?",
-           'writeCmd': lambda value: ":waveform:format %s" % (str(value)),
-           'writeValues': ['BYTE', 'BYT',
-                           'WORD', 'WOR',
-                           'ASCII', 'ASCI', 'ASC'],
-           })
-
-Attribute('WaveformOrigin',
+Attribute('Amplitude',
           {'type': PyTango.CmdArgType.DevDouble,
            'format': '%9.6f',
            'dim': [0],
-           'readCmd': ":WAVeform:YORigin?",
-           })
-
-Attribute('WaveformIncrement',
-          {'type': PyTango.CmdArgType.DevDouble,
-           'format': '%9.6f',
-           'dim': [0],
-           'readCmd': ":WAVeform:YINCrement?",
+           'readCmd': lambda ch, num: ":MEAS:VAMP? %s%d" % (ch, num),
+           'channels': True,
+           'functions': True,
            })
 
 Attribute('ByteOrder',
@@ -272,10 +79,40 @@ Attribute('ByteOrder',
            'memorized': True,
            })
 
+Attribute('CurrentSampleRate',
+          {'type': PyTango.CmdArgType.DevDouble,
+           'format': '%9.6f',
+           'dim': [0],
+           'readCmd': ":ACQuire:SRATe?",
+           })
+
 Attribute('Error',
           {'type': PyTango.CmdArgType.DevString,
            'dim': [0],
            'readCmd': ":SYSTem:ERRor?",
+           })
+
+Attribute('FallTime',
+          {'type': PyTango.CmdArgType.DevDouble,
+           'format': '%9.6f',
+           'dim': [0],
+           'readCmd': lambda ch, num: ":MEAS:FALLTIME? %s%d" % (ch, num),
+           'channels': True,
+           })
+
+Attribute('Frequency',
+          {'type': PyTango.CmdArgType.DevDouble,
+           'format': '%9.6f',
+           'dim': [0],
+           'readCmd': lambda ch, num: ":MEAS:FREQ? %s%d" % (ch, num),
+           'channels': True,
+           })
+
+Attribute('Impedance',
+          {'type': PyTango.CmdArgType.DevString,
+           'dim': [0],
+           'readCmd': lambda ch, num: ":%s%d:INPut?" % (ch, num),
+           'channels': True,
            })
 
 Attribute('Lock',
@@ -284,4 +121,167 @@ Attribute('Lock',
            'readCmd': "SYSTem:LOCK?",
            'writeCmd': lambda value: "SYSTem:LOCK %s" % (str(value)),
            'memorized': True,
+           })
+
+Attribute('Offset',
+          {'type': PyTango.CmdArgType.DevDouble,
+           'format': '%9.6f',
+           'dim': [0],
+           'readCmd': lambda ch, num: ":%s%d:OFFS?" % (ch, num),
+           'writeCmd': lambda ch, num: (lambda value: ":%s%d:OFFS %s"
+                                        % (ch, num, value)),
+           'channels': True,
+           })
+
+Attribute('OffsetH',
+          {'type': PyTango.CmdArgType.DevDouble,
+           'format': '%9.6f',
+           'dim': [0],
+           'readCmd': ":TIMebase:POS?",
+           'writeCmd': lambda value: ":TIMebase:POS %s" % (str(value)),
+           })
+
+Attribute('OverShoot',
+          {'type': PyTango.CmdArgType.DevDouble,
+           'format': '%9.6f',
+           'dim': [0],
+           'readCmd': lambda ch, num: ":MEAS:OVERSHOOT? %s%d" % (ch, num),
+           'channels': True,
+           })
+
+Attribute('Period',
+          {'type': PyTango.CmdArgType.DevDouble,
+           'format': '%9.6f',
+           'dim': [0],
+           'readCmd': lambda ch, num: ":MEAS:PERIOD? %s%d" % (ch, num),
+           'channels': True,
+           })
+
+Attribute('PreShoot',
+          {'type': PyTango.CmdArgType.DevDouble,
+           'format': '%9.6f',
+           'dim': [0],
+           'readCmd': lambda ch, num: ":MEAS:PRESHOOT? %s%d" % (ch, num),
+           'channels': True,
+           })
+
+Attribute('RiseTime',
+          {'type': PyTango.CmdArgType.DevDouble,
+           'format': '%9.6f',
+           'dim': [0],
+           'readCmd': lambda ch, num: ":MEAS:RISETIME? %s%d" % (ch, num),
+           'channels': True,
+           })
+
+Attribute('Scale',
+          {'type': PyTango.CmdArgType.DevDouble,
+           'format': '%9.6f',
+           'dim': [0],
+           'readCmd': lambda ch, num: ":%s%d:SCALe?" % (ch, num),
+           'writeCmd': lambda ch, num: (lambda value: ":%s%d:SCALe %s"
+                                        % (ch, num, value)),
+           'channels': True,
+           })
+
+Attribute('ScaleH',
+          {'type': PyTango.CmdArgType.DevDouble,
+           'format': '%9.6f',
+           'dim': [0],
+           'readCmd': ":TIMebase:SCALe?",
+           'writeCmd': lambda value: ":TIMebase:SCALe %s" % (str(value)),
+           })
+
+Attribute('State',
+          {'type': PyTango.CmdArgType.DevBoolean,
+           'dim': [0],
+           'readCmd': lambda ch, num: ":%s%d:DISPlay?" % (ch, num),
+           'writeCmd': lambda ch, num: (lambda value: ":%s%d:DISPlay %s"
+                                        % (ch, num, 'ON' if value else 'OFF')),
+           'channels': True,
+           'functions': True,
+           'manager': True,
+           })
+
+Attribute('TriggerType',
+          {'type': PyTango.CmdArgType.DevString,
+           'dim': [0],
+           'readCmd': ":TRIGger:MODE?",
+           'writeCmd': lambda value: ":TRIGger:MODE %s" % (str(value)),
+           })
+
+Attribute('VoltageLower',
+          {'type': PyTango.CmdArgType.DevDouble,
+           'format': '%9.6f',
+           'dim': [0],
+           'readCmd': lambda ch, num: ":MEAS:VLOWER? %s%d" % (ch, num),
+           'channels': True,
+           })
+
+Attribute('VoltageMax',
+          {'type': PyTango.CmdArgType.DevDouble,
+           'format': '%9.6f',
+           'dim': [0],
+           'readCmd': lambda ch, num: ":MEAS:VMAX? %s%d" % (ch, num),
+           'channels': True,
+           'functions': True,
+           })
+
+Attribute('VoltageMin',
+          {'type': PyTango.CmdArgType.DevDouble,
+           'format': '%9.6f',
+           'dim': [0],
+           'readCmd': lambda ch, num: ":MEAS:VMIN? %s%d" % (ch, num),
+           'channels': True,
+           'functions': True,
+           })
+
+Attribute('VoltageUpper',
+          {'type': PyTango.CmdArgType.DevDouble,
+           'format': '%9.6f',
+           'dim': [0],
+           'readCmd': lambda ch, num: ":MEAS:VUPPER? %s%d" % (ch, num),
+           'channels': True,
+           })
+
+Attribute('VPeakToPeak',
+          {'type': PyTango.CmdArgType.DevDouble,
+           'format': '%9.6f',
+           'dim': [0],
+           'readCmd': lambda ch, num: ":MEAS:VPP? %s%d" % (ch, num),
+           'channels': True,
+           'functions': True,
+           })
+
+Attribute('Waveform',
+          {'type': PyTango.CmdArgType.DevDouble,
+           'format': '%9.6f',
+           'dim': [1, 40000000],
+           'readCmd': lambda ch, num: ":WAVeform:SOURce %s%d;:WAVeform:DATA?"
+                                      % (ch, num),
+           'channels': True,
+           'functions': True,
+           })
+
+Attribute('WaveformDataFormat',
+          {'type': PyTango.CmdArgType.DevString,
+           'dim': [0],
+           'readCmd': ":waveform:format?",
+           'writeCmd': lambda value: ":waveform:format %s" % (str(value)),
+           'writeValues': ['BYTE', 'BYT',
+                           'WORD', 'WOR',
+                           'ASCII', 'ASCI', 'ASC'],
+           })
+
+Attribute('WaveformIncrement',
+          {'type': PyTango.CmdArgType.DevDouble,
+           'format': '%9.6f',
+           'dim': [0],
+           'readCmd': ":WAVeform:YINCrement?",
+           })
+
+Attribute('WaveformOrigin',
+          {'type': PyTango.CmdArgType.DevDouble,
+           'format': '%9.6f',
+           'dim': [0],
+           'readCmd': ":WAVeform:YORigin?",
            })
