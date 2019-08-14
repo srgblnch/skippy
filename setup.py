c9319f37f68464c1b939fa3f74c58af695986f59
Sergi Blanch-Torne <sblanch@cells.es>
2019-08-14 15:36:33 +0200

    working on classifiers

--- setup.py
+++ setup.py
@@ -50,8 +50,8 @@ classifiers = [
     'Development Status :: 5 - Production/Stable',
     'Intended Audience :: Developers',
     'Intended Audience :: Science/Research',
-#     'License :: OSI Approved :: '
-#     'GNU General Public License v3 or later (GPLv3+)',
+    'License :: OSI Approved :: '
+    'GNU General Public License v3 or later (GPLv3+)',
 #     'Operating System :: POSIX',
 #     'Programming Language :: Python :: 2.6',
 #     'Programming Language :: Python :: 2.7',
