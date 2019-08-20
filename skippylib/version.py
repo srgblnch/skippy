b62b6460362bc2c2ad6fba65b0aec3d7bfe77705
Sergi Blanch-Torne <sblanch@cells.es>
2019-08-20 09:54:35 +0200

    Bump version 1.4.7 to 1.5.0-alpha0

--- skippylib/version.py
+++ skippylib/version.py
@@ -20,7 +20,7 @@ __email__ = "sblanch@cells.es"
 __copyright__ = "Copyright 2015, CELLS / ALBA Synchrotron"
 __license__ = "GPLv3+"
 
-__version__ = '1.4.7'
+__version__ = '1.5.0-alpha0'
 
 
 def VERSION():
