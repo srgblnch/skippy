6914fa19afab6aa0cdc9a2fef7b618f9eb7b2983
Sergi Blanch-Torne <sblanch@cells.es>
2019-10-29 10:48:47 +0100

    convert the 'updateDynamicAttributes' command to be 'expert'

--- skippy/skippy.py
+++ skippy/skippy.py
@@ -1318,7 +1318,10 @@ class SkippyClass(PyTango.DeviceClass):
             [PyTango.DevBoolean, "none"]],
         'updateDynamicAttributes':
             [[PyTango.DevVoid, "none"],
-            [PyTango.DevBoolean, "none"]],
+            [PyTango.DevBoolean, "none"]
+            {
+                'Display level': PyTango.DispLevel.EXPERT,
+            } ],
         }
 
 
